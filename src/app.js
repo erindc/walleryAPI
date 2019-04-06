98ee433666f37360c5674a97481103fa56caf491
erindc <erindcox6@gmail.com>
2019-04-05 20:39:16 -0500

    organized routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg')
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-})
-
+const healthCheck = require ('./controllers/health');
+const imageController = require('./controllers/image/image_controller');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -15,31 +10,10 @@ const app = express();
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(healthCheck);
+app.use(imageController);
 
-// test db
-app.get('/api', (req, res) => {
-  (async () => {
-    const client = await pool.connect()
-    try {
-      const data = await client.query('SELECT * from images')
-      console.log(data)
-      res.status(200).json({ data:data.rows });
-    } finally {
-      client.release()
-    }
-  })().catch(e => {
-    console.log(e.stack)
-    res.status(500).json({ error: e });
-  });
-});
 
-app.get('/', (req, res) => {
-    try {
-      res.status(200).json({ message: 'ok' });
-    } catch (e) {
-      res.status(500).json({ error: e });
-    }
-});
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
 });
---
+++ src/controllers/image/image_controller.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
+
+router.get('/images', function(req, res) {
+  (async () => {
+    const client = await pool.connect()
+    try {
+      //TODO: determine if api will retrieve images from AWS S3
+      const data = await client.query('SELECT * from images')
+      res.status(200).json({ images: data.rows });
+    } finally {
+      client.release()
+    }
+  })().catch(e => {
+    console.log(e.stack)
+    res.status(500).json({ error: e });
+  });
+})
+
+//TODO uploads
+router.post('/images', function(req, res) {
+  res.status(200).json({ images: 'TODO' });
+})
+
+//TODO flag and like endpoint  (not working need to fix)
+router.post('/images/:id', function(req, res) {
+  res.status(200);
+})
+
+module.exports = router;
+
+
+
+
