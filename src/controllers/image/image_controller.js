20745c61a600bd08bd097d55cb1b963a12bef241
erindc <erindcox6@gmail.com>
2019-04-13 15:24:31 -0500

    added signed url trying to disply images on browser

--- src/controllers/image/image_controller.js
+++ src/controllers/image/image_controller.js
@@ -13,11 +13,30 @@ const pool = new Pool({
 
 router.get('/images', async function(req, res) {
     try {
-      const data = await pool.query('SELECT * FROM images ORDER BY likes DESC')
-      console.log(data);
-      res.status(200).json(data.rows);
+      var s3  = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: 'us-east-1',
+      });
+
+      const images = await pool.query('SELECT * FROM images ORDER BY likes DESC');
+
+      const fullImages = await new Promise((resolve, reject) => {
+        const imageWithLocs = images.rows.reduce((acc, image) => {
+            const params = {Bucket: process.env.BUCKETEER_BUCKET_NAME, Key: image.image_tag, Expires: 86400};
+            try {
+                const url = s3.getSignedUrl('getObject', params);
+                acc.push({...image, url});
+                return acc;
+            } catch (err) {
+                reject(err);
+            }
+        }, []);
+        resolve(imageWithLocs);
+      });
+      res.status(200).json(fullImages);
     } catch (err) {
-      console.error('Error during upload: ', err);
+      console.error('Error getting images: ', err);
       res.status(500).json({error: 'Internal error occured'});
     }
 })
@@ -26,13 +45,11 @@ router.post('/images', upload.single('walleryImage'), async function(req, res) {
   try {
     const file = req.file
 
-    let uuid = uuidv4();
-
     var params = {
-      Key: `public/${uuid}${file.originalname}`,
+      Key: `public/${uuidv4()}${file.originalname}`,
       Bucket: process.env.BUCKETEER_BUCKET_NAME,
       Body: file.buffer,
-      ContentType: "image/png"
+      ContentType: 'image/png'
     };
 
     var s3  = new AWS.S3({
@@ -51,10 +68,10 @@ router.post('/images', upload.single('walleryImage'), async function(req, res) {
       });
     })
 
-    const image = await pool.query('INSERT INTO images(user_id, image_tag, purchased, likes, flags, location) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
-    [uuidv4(), `public/${uuid}${file.originalname}`, false, 0, 0, `https://bucketeer-fd97f80b-c9ae-47af-9785-9ee39bb37e64.s3.amazonaws.com/${params.Key}`]);
+    await pool.query('INSERT INTO images(user_id, image_tag, purchased, likes, flags, location) VALUES($1, $2, $3, $4, $5, $6)',
+    [uuidv4(), `${params.Key}`, false, 0, 0, `https://bucketeer-fd97f80b-c9ae-47af-9785-9ee39bb37e64.s3.amazonaws.com/${params.Key}`]);
 
-    res.status(201).json(image.rows);
+    res.sendStatus(201);
  } catch (err) {
     console.error('Error during upload: ', err);
     res.status(500).json({error: 'Internal error occured'});
