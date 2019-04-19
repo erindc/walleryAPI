784580b74906f49d6d2d87cfcf0113947d24cd4a
erindc <erindcox6@gmail.com>
2019-04-19 17:24:17 -0500

    test updates

--- src/test/image_controller_test.js
+++ src/test/image_controller_test.js
@@ -7,29 +7,78 @@ const should = chai.should();
 const expect = chai.expect;
 const app = require('../app');
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-});
+const pool = new Pool();
 
 describe('Images', () => {
-  // beforeEach(async () => {
-  //   console.log(pool);
-  //   await pool.query('DELETE FROM images'); 
-  // });
+
+  beforeEach(async () => {
+    await pool.query('DELETE FROM images'); 
+  });
+
+  after(async () => {
+    await pool.query('DELETE FROM images'); 
+  });
 
   describe('GET images', () => {
     it('should get all images', async () => {
-      // await pool.query("INSERT INTO images(user_id, image_tag, purchased, likes, flags, location) VALUES ('testID', 'testImageTag', false, 0, 0, '/images/uploads/one'), ('testID2', 'testImageTag2', false, 0, 0, '/images/uploads/two')");
+      await pool.query("INSERT INTO images(user_id, image_tag, purchased, likes, flags, location) VALUES ('testID', 'testImageTag', false, 0, 0, '/images/uploads/one'), ('testID2', 'testImageTag2', false, 0, 0, '/images/uploads/two')");
       await request(app)
-        .get('/')
+        .get('/images/')
         .set('Content-Type', 'application/json')
         .expect((res) => {
-          console.log(res);
           res.status.should.equal(200);
+          res.body.should.have.lengthOf(2);
+        })
+    });
+  });
+
+  describe('PUT image', () => {
+    it('should increment image likes', async () => {
+      await pool.query("INSERT INTO images(user_id, image_tag, purchased, likes, flags, location) VALUES ('testID', 'testImageTag', false, 0, 0, '/images/uploads/one')");
+      await request(app)
+        .put('/images/testImageTag')
+        .set('Content-Type', 'application/json')
+        .send({likes:true})
+        .expect((res) => {
+          res.status.should.equal(204);
+        })
+        const image = await pool.query("SELECT * FROM images where image_tag='testImageTag'");
+        image.rows[0].likes.should.equal(1);
+        image.rows[0].flags.should.equal(0);
+    });
+
+    it('should increment image flags', async () => {
+      await pool.query("INSERT INTO images(user_id, image_tag, purchased, likes, flags, location) VALUES ('testID', 'testImageTag', false, 0, 0, '/images/uploads/one')");
+      await request(app)
+        .put('/images/testImageTag')
+        .set('Content-Type', 'application/json')
+        .send({flags:true})
+        .expect((res) => {
+          res.status.should.equal(204);
+        })
+        const image = await pool.query("SELECT * FROM images where image_tag='testImageTag'");
+        image.rows[0].likes.should.equal(0);
+        image.rows[0].flags.should.equal(1);
+    });
+  });
+
+  describe('POST image', async () => {
+    it('should insert image', async () => {
+      await request(app)
+        .post('/images')
+        .attach('walleryImage', './src/test/balloons.jpg')
+        .expect((res) => {
+          res.status.should.equal(201);
+        })
+    });
+
+    it('should not accept an invalid file extension', async () => {
+      await request(app)
+        .post('/images')
+        .attach('walleryImage', './src/test/invalid.txt')
+        .expect((res) => {
+          res.status.should.equal(400);
+          res.body.msg.should.equal('Invalid file type');
         })
     });
   });
